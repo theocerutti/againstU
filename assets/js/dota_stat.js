99f4c8c4ec3de1c4f0cc55ab11ea516d2288c9ea
theo <theo.cerutti@epitech.eu>
2019-02-11 09:04:57 +0100

    add steam-id-convertor module, can now get steamID64 / steamID32

--- assets/js/dota_stat.js
+++ assets/js/dota_stat.js
@@ -1,21 +1,19 @@
 function get_id_dota() {
     const username = $('#input_pseudo').val();
-    const country = $('#select_server').val();
-
-    console.log(username, country);
+    const server = $('#select_server').val();
 
     $.ajax({
         method: "POST",
         url: "http://localhost:8080/dota_stat",
         data: JSON.stringify({
             username,
-            country
+            server
         }),
         headers: {
             'content-type': 'application/json',
         },
         success: (response) => {
-            alert(response);
+            window.location.replace('dota_stat')
         }
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,67 @@
-const express = require('express');
-const axios = require('axios');
-const expressHbs = require('express-hbs');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const axios = require('axios')
+const expressHbs = require('express-hbs')
+const bodyParser = require('body-parser')
+const app = express()
+var BigNumber = require('bignumber.js')
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/../views');
-app.use(expressHbs.express4());
-app.use("/assets", express.static("./assets"));
-app.use(bodyParser.json());
+var stat_dota = {
+    id64: 0,
+    id32: '0',
+    username: 'not found',
+    server: 'not found',
+    success: -1
+}
+
+var convert = new BigNumber('76561197960265728');
+function to32(steamId64) {
+    steamId64 = new BigNumber(steamId64);
+    var steamId32 = steamId64.minus(convert);
+    return steamId32.toString();
+}
+
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/../views')
+app.use(expressHbs.express4())
+app.use("/assets", express.static("./assets"))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.redirect('/index');
+    res.redirect('/index')
 })
 
 app.get('/index', (req, res) => {
-    res.render('index');
+    res.render('index')
 })
 
 app.get('/dota', (req, res) => {
-    res.render('dota');
+    res.render('dota')
 })
 
-app.post('/dota', async (req, res) => {
-    const { body } = req;
-    console.log("j'ai recu une reponse")
-    var country;
-    console.log(body);
+app.post('/dota_stat', async (req, res) => {
+    const { body } = req
+    let server = 'not found'
     if (body.server === "EU West")
-        country = 'euw1';
-    else if (body_server == "North America")
-        country = 'na1';
-    await axios.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=291F6255D9427DB13DEEC82679CBFC87&vanityurl=" + body.username)
-        .then((response) => {
-        });
-    if (stat.status === 200) {
-        res.send('Player found');
-    }
+        server = 'euw1'
+    else if (body.server === "North America")
+        server = 'na1'
+    stat_dota.username = body.username
+    stat_dota.server = body.server
+    const res1_dota = await axios.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=291F6255D9427DB13DEEC82679CBFC87&vanityurl=" + body.username)
+    stat_dota.id64 = res1_dota.data.response.steamid
+    stat_dota.id32 = to32(stat_dota.id64)
+    stat_dota.success = res1_dota.data.response.success
+    // const res2_dota = await axios.get("")
+    if (stat_dota.success === 1)
+        res.send('Player found')
 })
 
 app.get('/dota_stat', (req, res) => {
-    res.render('dota_stat', { stat_dota });
+    res.render('dota_stat', { stat_dota })
 })
 
 app.use((req, res, next) => {
-    res.sendStatus(404).statusMessage;
+    res.sendStatus(404).statusMessage
 })
 
-app.listen(8080);
+app.listen(8080)
