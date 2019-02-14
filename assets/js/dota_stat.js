fb0306fd53c3da51f301fcfbf65262ea7e15d120
theo <theo.cerutti@epitech.eu>
2019-02-14 08:00:29 +0100

    delete server country choice

--- assets/js/dota_stat.js
+++ assets/js/dota_stat.js
@@ -1,13 +1,11 @@
 function get_id_dota() {
     const username = $('#input_pseudo').val();
-    const server = $('#select_server').val();
 
     $.ajax({
         method: "POST",
         url: "http://localhost:8080/dota_stat",
         data: JSON.stringify({
-            username,
-            server
+            username
         }),
         headers: {
             'content-type': 'application/json',
